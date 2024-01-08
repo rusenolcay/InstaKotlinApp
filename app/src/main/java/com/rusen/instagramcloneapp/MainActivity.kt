6e9362970c2e0e59e5f48df1def8365ae1b4713d
rusenolcay <narincrusen@gmail.com>
2024-01-09 02:46:56 +0300

    Register ekrani olusturuldu.

--- app/src/main/java/com/rusen/instagramcloneapp/MainActivity.kt
+++ app/src/main/java/com/rusen/instagramcloneapp/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.rusen.instagramcloneapp
 
+import android.content.Intent
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import com.rusen.instagramcloneapp.common.viewBinding
 import com.rusen.instagramcloneapp.databinding.ActivityMainBinding
 
@@ -12,5 +16,12 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+        window.statusBarColor = Color.TRANSPARENT
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, SignUpActivity::class.java))
+            finish()
+        }, 3000)
     }
 }
